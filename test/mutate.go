b69ae67bc69cc8776e357c1ecba3b72595cbf7b3
MrTizu <138714340+bl4cks3a@users.noreply.github.com>
2023-07-06 20:38:42 +0300

    common repo init

---
+++ test/mutate.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	. "github.com/nio-net/common"
+)
+
+func MutateByteSlice(bytez []byte) []byte {
+	if len(bytez) == 0 {
+		panic("Cannot mutate an empty bytez")
+	}
+	mBytez := make([]byte, len(bytez))
+	copy(mBytez, bytez)
+	bytez = mBytez
+
+	switch RandInt() % 2 {
+	case 0:
+		bytez[RandInt()%len(bytez)] += byte(RandInt()%255 + 1)
+	case 1:
+		pos := RandInt() % len(bytez)
+		bytez = append(bytez[:pos], bytez[pos+1:]...)
+	}
+	return bytez
+}
